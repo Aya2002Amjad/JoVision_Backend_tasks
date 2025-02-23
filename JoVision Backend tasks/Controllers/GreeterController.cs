c6f6004f9608ab7aecd3948a92cb9867354a5f01
Aya2002Amjad <aya2002amjad@gmail.com>
2025-02-23 16:53:02 +0300

    add task 44

---
+++ JoVision Backend tasks/Controllers/GreeterController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JoVision_Backend_tasks.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GreeterController : ControllerBase
+    {
+      
+         [HttpGet]
+         public IActionResult GetGreeting([FromQuery] string name = "anonymous")
+         {
+            if (name == "Aya" || name == "aya")
+                name = "Aya";
+            else name = "Anonymous";
+                return Ok($"Hello {name}");
+         }
+    
+       
+    }
+}
