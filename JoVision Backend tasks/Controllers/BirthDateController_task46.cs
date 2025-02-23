d362226b32814fbd9ca16c4d8afc38d4367cf248
Aya2002Amjad <aya2002amjad@gmail.com>
2025-02-23 17:12:34 +0300

    add task 46

---
+++ JoVision Backend tasks/Controllers/BirthDateController_task46.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JoVision_Backend_tasks.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BirthDateController_task46 : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult GetAge([FromForm] string name = "anonymous", [FromForm] string birthdate = null)
+        {
+            if (string.IsNullOrEmpty(birthdate) || !DateTime.TryParse(birthdate, out DateTime birthDateValue)) /// *Note*  date like this : 2002-10-15 
+            {
+                return Ok($"Hello {name}, I can’t calculate your age without knowing your birthdate!");
+            }
+
+            if (birthDateValue.Year < 1 || birthDateValue.Year >= 2025)
+            {
+                return BadRequest("Invalid birthdate! Year must be between 1 and 2025.");
+            }
+
+            int age = DateTime.UtcNow.Year - birthDateValue.Year;
+            if (DateTime.UtcNow < birthDateValue.AddYears(age))
+            {
+                age--;
+            }
+
+            return Ok($"Hello {name}, your age is {age}");
+        }
+    }
+}
